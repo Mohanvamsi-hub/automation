9c02f7956ece94c8fee2b23c8ca9cdcd0502bf2d
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-21 10:56:30 +0530

    path

--- mongo.sh
+++ mongo.sh
@@ -1,12 +1,8 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
 
-echo ${script_path}
-echo $app_user
-exit
-
-cp /home/centos/automation/mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y 
 sed -i -e 's|127.0.0.1|0.0.0.0|g' /etc/mongod.conf
 systemctl enable mongod 
-systemctl restart mongod 
+systemctl restart mongod
