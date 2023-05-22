76b5e08b9f77269d2240da55eca78cf71242bf25
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-22 11:07:53 +0530

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-echo location is ${0}
-echo first arg is ${1}
-echo second arg is ${2}
-echo all args is ${*}
-echo count of args is ${#}
+echo location is $0
+echo first arg is $1
+echo second arg is $2
+echo all args is $*
+echo count of args is $#
