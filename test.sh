b662697e1eb78cac1ca77cdaf2bb6769809ab887
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-23 19:06:02 +0530

    -z

--- test.sh
+++ test.sh
@@ -10,6 +10,12 @@ echo count of args is $#
 student=$1
 money=$2
 
+if [ -z $student ]
+then
+    echo Input is missing
+fi 
+
+
 if [ ${student} == "vamsi" ]
 then
     echo student name is ${student}, he has ${money} in his pocket
