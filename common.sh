c06f1cb84136549dc769b6c2c6dc64341575b879
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-23 23:03:41 +0530

    schemasetup

--- common.sh
+++ common.sh
@@ -12,15 +12,18 @@ printhead(){
 
 
 schema_setup(){
-    echo -e "\e[31m>>>>>>>> copying mongo repo <<<<<<<<\e[0m"
-    cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo
+    if [ -z "$schema_setup"=="mongo" ]
+    then
+        printhead "copying mongo repo"
+        cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo
 
-    echo -e "\e[31m>>>>>>>> Installing mongodb <<<<<<<<\e[0m"
-    yum install mongodb-org-shell -y
+        printhead "Installing mongodb"
+        yum install mongodb-org-shell -y
 
-    echo -e "\e[31m>>>>>>>> Loading schema <<<<<<<<\e[0m"
-    mongo --host mongodb-dev.kmvdevops.online </app/schema/${component}.js
-}
+        printhead "Loading schema"
+        mongo --host mongodb-dev.kmvdevops.online </app/schema/${component}.js
+    fi
+}1
 
 
 
