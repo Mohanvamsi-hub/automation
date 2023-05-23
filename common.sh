66ab94f26edde4c71454cf1bf063d9c2dcafb3fc
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-23 11:21:57 +0530

    printhead

--- common.sh
+++ common.sh
@@ -2,35 +2,39 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
+printhead(){
+    echo -e "\e[31m>>>>>>>> $*  <<<<<<<<\e[0m"
+}
+
 func_nodejs(){
-    
-echo -e "\e[31m>>>>>>>> Setup NodeJS repo <<<<<<<<\e[0m"
+
+printhead "Setup NodeJS repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[31m>>>>>>>> Installing nodejs <<<<<<<<\e[0m"
+printhead "Installing nodejs"
 yum install nodejs -y
 
-echo -e "\e[31m>>>>>>>> Adding user and creating a directory <<<<<<<<\e[0m"
+printhead "Adding user and creating a directory"
 useradd ${app_user}
 rm -rf /app
 mkdir /app 
 
-echo -e "\e[31m>>>>>>>> Setup code repo <<<<<<<<\e[0m"
+printhead "Setup code repo"
 curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
 
-echo -e "\e[31m>>>>>>>> Moving into the app directory <<<<<<<<\e[0m"
+printhead "Moving into the app directory"
 cd /app 
 
-echo -e "\e[31m>>>>>>>> unzipping code content <<<<<<<<\e[0m"
+printhead "unzipping code content"
 unzip /tmp/${component}.zip
 
-echo -e "\e[31m>>>>>>>> Installing nodejs dependancies <<<<<<<<\e[0m"
+printhead "Installing nodejs dependancies"
 npm install 
 
-echo -e "\e[31m>>>>>>>> Copying service file <<<<<<<<\e[0m"
+printhead "Copying service file"
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-echo -e "\e[31m>>>>>>>> Starting the service <<<<<<<<\e[0m"
+printhead "Starting the service"
 systemctl daemon-reload
 systemctl enable ${component} 
 systemctl restart ${component}
