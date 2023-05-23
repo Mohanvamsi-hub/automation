1c958fa0334efe8cc4e63361b7f9a6b945b4dde6
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-23 19:27:00 +0530

    pay

--- payment.sh
+++ payment.sh
@@ -3,7 +3,7 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_user_password=$1
 
-if [ -z $rabbitmq_user_password ]
+if [ -z ${rabbitmq_user_password} ]
 then
     echo Input password is missing
     exit 
