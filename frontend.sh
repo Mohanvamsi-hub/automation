68cecdede77a6fa565f0c27b66ec978940b06468
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-29 23:10:03 +0530

    frontend setup

--- frontend.sh
+++ frontend.sh
@@ -4,28 +4,28 @@ source ${script_path}/common.sh
 
 
 
-printhead "Installing Maven"
+printhead "Installing nginx"
 yum install nginx -y &>>$log_file
 func_stat_check $?
 
-printhead "Installing Maven"
+printhead "copying config file"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
 func_stat_check $?
 
-printhead "Installing Maven"
+printhead "removing old app content"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 func_stat_check $?
 
-printhead "Installing Maven"
+printhead "Download code content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>$log_file
 cd /usr/share/nginx/html  &>>$log_file
 func_stat_check $?
 
-printhead "Installing Maven"
+printhead "Extracting app content"
 unzip /tmp/frontend.zip &>>$log_file
 func_stat_check $?
 
-printhead "Installing Maven"
+printhead "starting nginx service"
 systemctl enable nginx 
 systemctl restart nginx
 func_stat_check $?
