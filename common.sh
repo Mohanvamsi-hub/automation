55fe0dd51fcd2c0324c93524e4bf89dca9586908
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-29 22:36:34 +0530

    $?

--- common.sh
+++ common.sh
@@ -11,7 +11,7 @@ printhead(){
 }
 
 func_stat_check(){
-    if [ $? -eq 0 ]
+    if [ $1 -eq 0 ]
     then
         echo -e "\e[35m>>>>>> SUCCESS <<<<<<\e[0m"
     else
@@ -26,17 +26,17 @@ func_schema_setup(){
         printhead "copying mongo repo"
         cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo &>>$log_file
 
-        func_stat_check
+        func_stat_check $?
 
         printhead "Installing mongodb"
         yum install mongodb-org-shell -y &>>$log_file
 
-        func_stat_check
+        func_stat_check $?
 
         printhead "Loading schema"
         mongo --host mongodb-dev.kmvdevops.online </app/schema/${component}.js &>>$log_file
 
-        func_stat_check
+        func_stat_check $?
     fi
     if [ "$schema_setup" == "mysql" ]
     then
@@ -44,7 +44,7 @@ func_schema_setup(){
         yum install mysql -y  &>>$log_file
         mysql -h mysql-dev.kmvdevops.online -uroot -p${mysql_password} < /app/schema/${component}.sql &>>$log_file
 
-        func_stat_check
+        func_stat_check $?
     fi
 }
 
@@ -58,32 +58,32 @@ func_app_prereq(){
     rm -rf /app
     mkdir /app &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 
     printhead "downloading code content"
     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 
     printhead "unzipping content in app dir"
     cd /app 
     unzip /tmp/${component}.zip &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 }
 
 func_systemd_setup(){
 
     cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 
     printhead "starting the service"
     systemctl daemon-reload
     systemctl enable ${component} 
     systemctl restart ${component}
 
-    func_stat_check
+    func_stat_check $?
 
 }
 
@@ -92,19 +92,19 @@ func_nodejs(){
     printhead "Setup NodeJS repo"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 
     printhead "Installing nodejs"
     yum install nodejs -y &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 
     func_app_prereq
 
     printhead "Installing nodejs dependancies"
     npm install &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 
     func_systemd_setup
     func_schema_setup
@@ -116,7 +116,7 @@ func_java(){
     printhead "Installing Maven"
     yum install maven -y &>> /tmp/roboshop.log
 
-    func_stat_check
+    func_stat_check $?
 
     func_app_prereq
 
@@ -124,7 +124,7 @@ func_java(){
     mvn clean package &>>$log_file
     mv target/${component}-1.0.jar ${component}.jar &>>$log_file
 
-    func_stat_check
+    func_stat_check $?
 
     func_systemd_setup
 
