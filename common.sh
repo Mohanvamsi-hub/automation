445cab5e25167e39ae785a024c2bd6e5606d2b2d
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-29 23:06:56 +0530

    frontend setup

--- common.sh
+++ common.sh
@@ -25,17 +25,14 @@ func_schema_setup(){
     then
         printhead "copying mongo repo"
         cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo &>>$log_file
-
         func_stat_check $?
 
         printhead "Installing mongodb"
         yum install mongodb-org-shell -y &>>$log_file
-
         func_stat_check $?
 
         printhead "Loading schema"
         mongo --host mongodb-dev.kmvdevops.online </app/schema/${component}.js &>>$log_file
-
         func_stat_check $?
     fi
     if [ "$schema_setup" == "mysql" ]
@@ -43,7 +40,6 @@ func_schema_setup(){
         printhead "Installing mysql"
         yum install mysql -y  &>>$log_file
         mysql -h mysql-dev.kmvdevops.online -uroot -p${mysql_password} < /app/schema/${component}.sql &>>$log_file
-
         func_stat_check $?
     fi
 }
@@ -63,32 +59,27 @@ func_app_prereq(){
     printhead "creating a diretory"
     rm -rf /app
     mkdir /app &>>$log_file
-
     func_stat_check $?
 
     printhead "downloading code content"
     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
-
     func_stat_check $?
 
     printhead "unzipping content in app dir"
     cd /app 
     unzip /tmp/${component}.zip &>>$log_file
-
     func_stat_check $?
 }
 
 func_systemd_setup(){
 
     cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
-
     func_stat_check $?
 
     printhead "starting the service"
     systemctl daemon-reload
     systemctl enable ${component} 
     systemctl restart ${component}
-
     func_stat_check $?
 
 }
@@ -97,19 +88,16 @@ func_nodejs(){
 
     printhead "Setup NodeJS repo"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
-
     func_stat_check $?
 
     printhead "Installing nodejs"
     yum install nodejs -y &>>$log_file
-
     func_stat_check $?
 
     func_app_prereq
 
     printhead "Installing nodejs dependancies"
     npm install &>>$log_file
-
     func_stat_check $?
 
     func_systemd_setup
@@ -121,15 +109,13 @@ func_java(){
     
     printhead "Installing Maven"
     yum install maven -y &>>$log_file
-
     func_stat_check $?
-
+    
     func_app_prereq
 
     printhead "downloading dependencies"
     mvn clean package &>>$log_file
     mv target/${component}-1.0.jar ${component}.jar &>>$log_file
-
     func_stat_check $?
 
     func_systemd_setup
--- frontend.sh
+++ frontend.sh
@@ -2,11 +2,30 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-yum install nginx -y 
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
-systemctl restart nginx 
+
+
+printhead "Installing Maven"
+yum install nginx -y &>>$log_file
+func_stat_check $?
+
+printhead "Installing Maven"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_stat_check $?
+
+printhead "Installing Maven"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+func_stat_check $?
+
+printhead "Installing Maven"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>$log_file
+cd /usr/share/nginx/html  &>>$log_file
+func_stat_check $?
+
+printhead "Installing Maven"
+unzip /tmp/frontend.zip &>>$log_file
+func_stat_check $?
+
+printhead "Installing Maven"
 systemctl enable nginx 
+systemctl restart nginx
+func_stat_check $?
