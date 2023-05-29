279dfd49b983d53409ee2755e99626c2b91c08c8
Mohanvamsi-hub <mohanvamsi107@gmail.com>
2023-05-29 21:38:15 +0530

    stat_check

--- common.sh
+++ common.sh
@@ -9,7 +9,15 @@ printhead(){
     echo -e "\e[31m>>>>>>>> $*  <<<<<<<<\e[0m"
 }
 
-
+func_stat_check(){
+    if [ $? -eq 0 ]
+    then
+        echo -e "\e[35m>>>>>> SUCCESS <<<<<<\e[0m"
+    else
+        echo -e "\e[35m>>>>>> FAILURE <<<<<<\e[0m"
+        exit 1
+    fi
+}
 
 func_schema_setup(){
     if [ "$schema_setup" == "mongo" ]
@@ -17,17 +25,25 @@ func_schema_setup(){
         printhead "copying mongo repo"
         cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo
 
+        func_stat_check
+
         printhead "Installing mongodb"
         yum install mongodb-org-shell -y
 
+        func_stat_check
+
         printhead "Loading schema"
         mongo --host mongodb-dev.kmvdevops.online </app/schema/${component}.js
+
+        func_stat_check
     fi
     if [ "$schema_setup" == "mysql" ]
     then
         printhead "Installing mysql"
         yum install mysql -y 
         mysql -h mysql-dev.kmvdevops.online -uroot -p${mysql_password} < /app/schema/${component}.sql 
+
+        func_stat_check
     fi
 }
 
@@ -41,22 +57,32 @@ printhead "creating a diretory"
 rm -rf /app
 mkdir /app 
 
+func_stat_check
+
 printhead "downloading code content"
 curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+
+func_stat_check
 
 printhead "unzipping content in app dir"
 cd /app 
 unzip /tmp/${component}.zip
+
+func_stat_check
 }
 
 func_systemd_setup(){
 
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
+func_stat_check
+
 printhead "starting the service"
 systemctl daemon-reload
 systemctl enable ${component} 
 systemctl restart ${component}
+
+func_stat_check
 
 }
 
@@ -65,13 +91,19 @@ func_nodejs(){
 printhead "Setup NodeJS repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
+func_stat_check
+
 printhead "Installing nodejs"
 yum install nodejs -y
+
+func_stat_check
 
 func_app_prereq
 
 printhead "Installing nodejs dependancies"
 npm install 
+
+func_stat_check
 
 func_systemd_setup
 func_schema_setup
@@ -83,13 +115,7 @@ func_java(){
 printhead "Installing Maven"
 yum install maven -y
 
-if [ $? -eq 0 ]
-then
-    echo -e "\e[35m>>>>>> SUCCESS <<<<<<\e[0m"
-else
-    echo -e "\e[35m>>>>>> FAILURE <<<<<<\e[0m"
-    exit 1
-fi
+func_stat_check
 
 func_app_prereq
 
@@ -97,13 +123,7 @@ printhead "downloading dependencies"
 mvn clean package 
 mv target/${component}-1.0.jar ${component}.jar
 
-if [ $? -eq 0 ]
-then
-    echo -e "\e[35m>>>>>> SUCCESS <<<<<<\e[0m"
-else
-    echo -e "\e[35m>>>>>> FAILURE <<<<<<\e[0m"
-    exit 1
-fi
+func_stat_check
 
 func_systemd_setup
 
